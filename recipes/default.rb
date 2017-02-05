d243555b347be91fe0a94236b632fcf60518cb3f
Alex Markessinis <markea@sage.edu>
2017-02-04 22:03:01 -0500

    trying to fix how ignore failures would work

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,148 @@
 # Recipe:: default
 #
 
+# This function managed the service
+def manage_service(
+    ignore_fail_option,
+    service_name_option,
+    actions_option,
+    notifies_option,
+    init_cmd_option,
+    pattern_option,
+    priority_option,
+    provider_option,
+    reload_cmd_option,
+    restart_cmd_option,
+    retries_option,
+    start_cmd_option,
+    status_cmd_option,
+    stop_cmd_option,
+    subscribes_option,
+    supports_option,
+    timeout_option
+
+)
+# After gathering all of the options define the service here.
+  service service do
+
+    # Set the action(s)
+    action actions_option
+
+    # Set the ignore failure option
+    ignore_failure ignore_fail_option
+
+    # Set the init command directive if needed
+    if init_cmd_option
+      init_command init_cmd_option
+    end
+
+    # Set the notifies option if needed
+    if notifies_option
+      if notifies_option['action'] && notifies_option['resource']
+        if notifies_option['timer']
+          notifies notifies_option['action'].to_s.to_sym, notifies_option['resource'].to_s, notifies_option['timer'].to_s.to_sym
+        else
+          notifies notifies_option['action'].to_s.to_sym, notifies_option['resource'].to_s, :delayed
+        end
+      end
+    end
+
+    # Set the pattern option if needed
+    if pattern_option
+      unless pattern_option == service_name_option
+        pattern pattern_option
+      end
+    end
+
+    # If on debian set the priority directive if needed
+    if node['platform'] == 'debian'
+      if priority_option
+        priority priority_option
+      end
+    end
+
+    # Set the provider directive if needed
+    if provider_option
+      provider provider_option
+    end
+
+    # Set the reload command directive if needed
+    if reload_cmd_option
+      reload_command reload_cmd_option
+    end
+
+    # Set the restart command directive if needed
+    if restart_cmd_option
+      restart_command restart_cmd_option
+    end
+
+    # Set the retries directive if needed
+    if retries_option
+      retries retries_option
+    end
+
+    # Set the retry delay directive if needed
+    if retry_delay_option
+      retry_delay retry_delay_option
+    end
+
+    # Set the service name option
+    if service_name_option
+      service_name service_name_option
+    end
+
+    # Set the start command directive if needed
+    if start_cmd_option
+      start_command start_cmd_option
+    end
+
+    # Set the status command directive if needed
+    if status_cmd_option
+      status_command status_cmd_option
+    end
+
+    # Set the stop command directive if needed
+    if stop_cmd_option
+      stop_command stop_cmd_option
+    end
+
+    # Set the subscribes directive if needed
+    if subscribes_option
+      if subscribes_option['action'] && subscribes_option['resource']
+        if subscribes_option['timer']
+          subscribes subscribes_option['action'].to_s.to_sym, subscribes_option['resource'], subscribes_option['timer'].to_s.to_sym
+        else
+          subscribes subscribes_option['action'].to_s.to_sym, subscribes_option['resource'], :delayed
+        end
+      end
+    end
+
+    # Set the supports directive if needed
+    if supports_option
+      s = {}
+      supports_option.each do |symbol, value|
+        s[symbol.to_s.to_sym] = value
+      end
+      unless s.empty?
+        supports s
+      end
+    end
+
+    # Set the timeout directive if needed
+    if node['platform'] == 'windows'
+      if timeout_option
+        timeout timeout_option
+      end
+    end
+
+  end
+end
+
+
+
+
 # Global value for ignoring failures
-ignore_failure_option = node['manage_services']['ignore_failure']
+ignore_fail_option = node['manage_services']['ignore_failure']
 
 if node['manage_services']['services']
 
@@ -56,13 +196,13 @@ if node['manage_services']['services']
 
     # If the service has ignore failure overridden, set it.
     unless service_options['ignore_failure'].nil?
-      ignore_failure_option = service_options['ignore_failure']
+      ignore_fail_option = service_options['ignore_failure']
     end
 
     # If we need to grab the init command
-    init_command_option = false
+    init_cmd_option = false
     if service_options['init_command']
-      init_command_option = service_options['init_command']
+      init_cmd_option = service_options['init_command']
     end
 
     # Determine if there is anything to notify
@@ -92,15 +232,15 @@ if node['manage_services']['services']
     end
 
     # Grab the reload command
-    reload_command_option = false
+    reload_cmd_option = false
     if service_options['reload_command']
-      reload_command_option = service_options['reload_command']
+      reload_cmd_option = service_options['reload_command']
     end
 
     # Grab the restart command
-    restart_command_option = false
+    restart_cmd_option = false
     if service_options['restart_command']
-      restart_command_option = service_options['restart_command']
+      restart_cmd_option = service_options['restart_command']
     end
 
     # Grab the retries option
@@ -116,21 +256,21 @@ if node['manage_services']['services']
     end
 
     # Grab the start command option
-    start_command_option = false
+    start_cmd_option = false
     if service_options['start_command']
-      start_command_option = service_options['start_command']
+      start_cmd_option = service_options['start_command']
     end
 
     # Grab the status command option
-    status_command_option = false
+    status_cmd_option = false
     if service_options['status_command']
-      status_command_option = service_options['status_command']
+      status_cmd_option = service_options['status_command']
     end
 
     # Grab the stop command option
-    stop_command_option = false
+    stop_cmd_option = false
     if service_options['stop_command']
-      stop_command_option = service_options['stop_command']
+      stop_cmd_option = service_options['stop_command']
     end
 
     # Grab the subscribes option
@@ -154,119 +294,53 @@ if node['manage_services']['services']
       end
     end
 
-    # After gathering all of the options define the service here.
-    service service do
-
-      # Set the action(s)
-      action actions_option
-
-      # Set the ignore failure option
-      ignore_failure ignore_failure_option
-
-      # Set the init command directive if needed
-      if init_command_option
-        init_command init_command_option
-      end
-
-      # Set the notifies option if needed
-      if notifies_option
-        if notifies_option['action'] && notifies_option['resource']
-          if notifies_option['timer']
-            notifies notifies_option['action'].to_s.to_sym, notifies_option['resource'].to_s, notifies_option['timer'].to_s.to_sym
-          else
-            notifies notifies_option['action'].to_s.to_sym, notifies_option['resource'].to_s, :delayed
-          end
+    if ignore_fail_option
+      begin
+        manage_service(
+            ignore_fail_option,
+            service_name_option,
+            actions_option,
+            notifies_option,
+            init_cmd_option,
+            pattern_option,
+            priority_option,
+            provider_option,
+            reload_cmd_option,
+            restart_cmd_option,
+            retries_option,
+            start_cmd_option,
+            status_cmd_option,
+            stop_cmd_option,
+            subscribes_option,
+            supports_option,
+            timeout_option
+        )
+      rescue
+        log 'Services' do
+          message "Service resource failed."
+          level :warn
         end
       end
-
-      # Set the pattern option if needed
-      if pattern_option
-        unless pattern_option == service_name_option
-          pattern pattern_option
-        end
-      end
-
-      # If on debian set the priority directive if needed
-      if node['platform'] == 'debian'
-        if priority_option
-          priority priority_option
-        end
-      end
-
-      # Set the provider directive if needed
-      if provider_option
-        provider provider_option
-      end
-
-      # Set the reload command directive if needed
-      if reload_command_option
-        reload_command reload_command_option
-      end
-
-      # Set the restart command directive if needed
-      if restart_command_option
-        restart_command restart_command_option
-      end
-
-      # Set the retries directive if needed
-      if retries_option
-        retries retries_option
-      end
-
-      # Set the retry delay directive if needed
-      if retry_delay_option
-        retry_delay retry_delay_option
-      end
-
-      # Set the service name option
-      if service_name_option
-        service_name service_name_option
-      end
-
-      # Set the start command directive if needed
-      if start_command_option
-        start_command start_command_option
-      end
-
-      # Set the status command directive if needed
-      if status_command_option
-        status_command status_command_option
-      end
-
-      # Set the stop command directive if needed
-      if stop_command_option
-        stop_command stop_command_option
-      end
-
-      # Set the subscribes directive if needed
-      if subscribes_option
-        if subscribes_option['action'] && subscribes_option['resource']
-          if subscribes_option['timer']
-            subscribes subscribes_option['action'].to_s.to_sym, subscribes_option['resource'], subscribes_option['timer'].to_s.to_sym
-          else
-            subscribes subscribes_option['action'].to_s.to_sym, subscribes_option['resource'], :delayed
-          end
-        end
-      end
-
-      # Set the supports directive if needed
-      if supports_option
-        s = {}
-        supports_option.each do |symbol, value|
-          s[symbol.to_s.to_sym] = value
-        end
-        unless s.empty?
-          supports s
-        end
-      end
-
-      # Set the timeout directive if needed
-      if node['platform'] == 'windows'
-        if timeout_option
-          timeout timeout_option
-        end
-      end
-
+    else
+      manage_service(
+          ignore_fail_option,
+          service_name_option,
+          actions_option,
+          notifies_option,
+          init_cmd_option,
+          pattern_option,
+          priority_option,
+          provider_option,
+          reload_cmd_option,
+          restart_cmd_option,
+          retries_option,
+          start_cmd_option,
+          status_cmd_option,
+          stop_cmd_option,
+          subscribes_option,
+          supports_option,
+          timeout_option
+      )
     end
   end
 else
